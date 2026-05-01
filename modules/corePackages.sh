b7a7d99f6f592004846d9c5e1faf20318dd55486
mnojz <manoj.joshi.public@gmail.com>
2026-05-01 16:36:26 +0545

    added some more core packages

--- modules/corePackages.sh
+++ modules/corePackages.sh
@@ -6,8 +6,8 @@ PACMAN_PKGS=(
     fish fastfetch curl wget unzip p7zip
     bluetui acpi net-tools neovim mpv nautilus
     file-roller kdeconnect evtest xorg-xev
-    ffmpegthumbnailer resvg nerd-fonts
-    imv android-tools raylib decibels
+    ffmpegthumbnailer resvg nerd-fonts zed
+    imv android-tools raylib decibels pdfarranger
     eza gedit gnome-system-monitor archlinux-xdg-menu
     gpu-screen-recorder evince kwrite
     zoxide ripgrep fzf cliphist polkit-gnome
@@ -17,7 +17,7 @@ PACMAN_PKGS=(
 AUR_PKGS=(
     helium-browser-bin
     visual-studio-code-bin
-    apple-fonts
+    apple-fonts pacseek
     font-noto-cjk
     qt6ct-kde
     darkly-bin
