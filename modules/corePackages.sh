acf8564b23920dd02cd642294deb732e9b5516f1
manoj joshi <manoj.joshi.public@gmail.com>
2026-05-31 10:13:52 +0545

    packages updated

--- modules/corePackages.sh
+++ modules/corePackages.sh
@@ -3,23 +3,23 @@
 section "Installing core packages"
 
 PACMAN_PKGS=(
-    fish fastfetch curl wget unzip p7zip polkit-gnome
-    bluetui acpi net-tools neovim mpv cliphist fzf rsync
-    kdeconnect sshfs evtest xorg-xev ripgrep zoxide evince
-    ffmpegthumbnailer resvg nerd-fonts mousepad gnome-keyring
-    imv android-tools raylib decibels pdfarranger xdg-user-dirs 
-    eza archlinux-xdg-menu gpu-screen-recorder plymouth ark
+    fish fastfetch curl wget unzip p7zip polkit-gnome gnome-software 
+    bluetui acpi net-tools neovim mpv cliphist fzf rsync mousepad 
+    kdeconnect sshfs evtest xorg-xev ripgrep zoxide evince gnome-calculator
+    ffmpegthumbnailer resvg nerd-fonts mousepad gnome-keyring flatpak 
+    imv android-tools raylib decibels pdfarranger xdg-user-dirs libreoffice
+    eza archlinux-xdg-menu gpu-screen-recorder plymouth ark pdfarranger 
     intel-media-driver libva-nvidia-driver libva-utils starship
-    hyprpicker 
+    hyprpicker yazi wine winetricks qbittorrent cava gaphor ghex
 )
 
 AUR_PKGS=(
     brave-origin-beta-bin
     visual-studio-code-bin
-    apple-fonts pacseek
-    font-noto-cjk frameworkintegration darkly-bin
+    apple-fonts pacseek peaclock
+    frameworkintegration darkly-bin
     qt6ct-kde kora-icon-theme
-    sddm-silent-theme
+    sddm-silent-theme unimatrix 
 )
 
 install_pacman "${PACMAN_PKGS[@]}"
