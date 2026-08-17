0b4407482674b1fec1b0719a0edb224daaa25c41
mnojz <manoj.joshi.public@gmail.com>
2026-08-17 21:01:56 +0545

    libreoffice out onlyoffice in

--- modules/corePackages.sh
+++ modules/corePackages.sh
@@ -7,7 +7,7 @@ PACMAN_PKGS=(
     bluetui acpi net-tools neovim mpv cliphist fzf rsync mousepad 
     kdeconnect sshfs evtest xorg-xev ripgrep zoxide evince gnome-calculator
     ffmpegthumbnailer resvg nerd-fonts mousepad gnome-keyring flatpak 
-    imv android-tools raylib decibels pdfarranger xdg-user-dirs libreoffice
+    imv android-tools raylib decibels pdfarranger xdg-user-dirs onlyoffice-bin
     eza archlinux-xdg-menu gpu-screen-recorder plymouth ark pdfarranger 
     intel-media-driver libva-nvidia-driver libva-utils starship man
     hyprpicker yazi wine winetricks qbittorrent cava gaphor ghex linux-headers
