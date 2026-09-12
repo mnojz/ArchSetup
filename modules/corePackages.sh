f7f9067b7ef574037f096b3617d9ddf72fd834db
mnojz <manoj.joshi.public@gmail.com>
2026-09-12 11:17:02 +0545

    pkg up

--- modules/corePackages.sh
+++ modules/corePackages.sh
@@ -7,7 +7,7 @@ PACMAN_PKGS=(
     bluetui acpi net-tools neovim mpv cliphist fzf mousepad 
     kdeconnect sshfs evtest xorg-xev ripgrep zoxide evince gnome-calculator
     ffmpegthumbnailer resvg nerd-fonts mousepad gnome-keyring flatpak 
-    imv android-tools raylib decibels pdfarranger xdg-user-dirs onlyoffice-bin
+    eog android-tools raylib amberol pdfarranger xdg-user-dirs onlyoffice-bin
     eza archlinux-xdg-menu gpu-screen-recorder plymouth ark pdfarranger 
     intel-media-driver libva-nvidia-driver libva-utils starship man
     hyprpicker yazi wine winetricks qbittorrent cava gaphor ghex linux-headers
